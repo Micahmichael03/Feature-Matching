7c1c00683f35f7ae0f2421be045caa8635c4e4c5
Michael <Makoflash03@gmail.com>
2025-02-05 14:09:24 +0100

    main_code

--- main.py
+++ main.py
@@ -21,4 +21,39 @@ def Feature_matching():
         return
 
     orb = cv2.ORB_create()
-    kp1, des1 = orb.detectAndCompute(img1, None)
+    keypoints1, descriptors1 = orb.detectAndCompute(img1, None)
+    keypoints2, descriptors2 = orb.detectAndCompute(img2, None)
+
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    matches = bf.match(descriptors1, descriptors2)
+
+    matches = sorted(matches, key = lambda x:x.distance)
+
+    img_matches = cv2.drawMatches(img1, keypoints1, img2, keypoints2, matches[:50], None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+
+    cv2.imshow("Matches", img_matches)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+root = Tk()
+root.title('Feature Matching')
+
+img1 = None
+img2 = None
+
+btn_select_image1 = Button(root, text="Select Image 1", command=select_image)
+btn_select_image1.pack()
+
+label_img = Label(root, text="Image 1: Not selected")
+label_img.pack()
+
+btn_select_image2 = Button(root, text="Select Image 2", command=select_image_2)
+btn_select_image2.pack()
+
+label_img_2 = Label(root, text="Image 2: Not selected")
+label_img_2.pack()
+
+btn_match_features = Button(root, text="Match Features", command=Feature_matching)
+btn_match_features.pack()
+
+root.mainloop()
